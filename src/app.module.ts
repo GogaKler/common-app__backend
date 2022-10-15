0cef6f8c978d67cad120dd944daba3d1f8698c9a
Egor Kolesnikov <gogakler@gmail.com>
2022-10-15 21:38:00 +0300

    auth wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { UsersController } from './modules/users/users.controller';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, AuthModule],
     controllers: [AppController, UsersController],
     providers: [AppService]
 })
